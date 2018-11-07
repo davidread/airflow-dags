0cd11b8d6453bd3d7c91f19644bd981b59b469ee
Karik Isichei <karik.isichei@gmail.com>
2018-11-07 10:23:56 +0000

    Updating to new image and including database building with ETL manager

--- mags_raw_to_processed_fast_forward.py
+++ mags_raw_to_processed_fast_forward.py
@@ -5,12 +5,11 @@ from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 
-TAR_S3_RELATIVE_FOLDER_PATHS = ""
 TAR_PROCESS_SCRIPT = "tar_raw_to_process.py"
 TAR_DB_SCRIPT = "rebuild_tar_databases.py"
 DB_VERSION = 'v1'
 
-# HOCAS_RAW_FOLDER = "s3://mojap-raw/hmcts/hocas/"
+# HOCAS_RAW_FOLDER = ""
 # HOCAS_PYTHON_SCRIPT_NAME = "hocas_raw_to_process.py"
 
 MAGS_RAW_TO_PROCESSED_IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-magistrates-data-engineering:v0.0.5"
@@ -25,14 +24,26 @@ task_args = {
 
 # Catch-up on dates before today-REUPDATE_LAST_N_DAYS days
 dag = DAG(
-    "mags_data_raw_to_processed",
+    "mags_data_raw_to_processed_fast_forward",
     default_args=task_args,
-    description="Process mags data (HOCAS and TAR)",
+    description="Process mags data (HOCAS and TAR) running all preprocessing in parallel",
     start_date=datetime.now(),
     schedule_interval=None,
 )
 
 tasks = {}
+
+file_land_timestamps = [
+"file_land_timestamp=1541030400",
+"file_land_timestamp=1541116800",
+"file_land_timestamp=1541203200",
+"file_land_timestamp=1541289600",
+"file_land_timestamp=1541376000",
+"file_land_timestamp=1541462400",
+"file_land_timestamp=1541548800",
+"file_land_timestamp=1541635200",
+"file_land_timestamp=1541721600"
+]
 
 task_id = "rebuild-athena-schemas"
 tasks[task_id] = KubernetesPodOperator(
@@ -52,24 +63,27 @@ tasks[task_id] = KubernetesPodOperator(
     annotations={"iam.amazonaws.com/role": MAGS_RAW_TO_PROCESSED_ROLE},
 )
 
-task_id = "process-tar"
-tasks[task_id] = KubernetesPodOperator(
-    dag=dag,
-    namespace="airflow",
-    image=MAGS_RAW_TO_PROCESSED_IMAGE,
-    env_vars={
-        "PYTHON_SCRIPT_NAME": TAR_PROCESS_SCRIPT,
-        "S3_RELATIVE_FOLDER_PATHS": TAR_S3_RELATIVE_FOLDER_PATHS,
-        "DB_VERSION": DB_VERSION
-    },
-    arguments=["{{ ds }}"],
-    labels={"app": dag.dag_id},
-    name=task_id,
-    in_cluster=True,
-    task_id=task_id,
-    get_logs=True,
-    annotations={"iam.amazonaws.com/role": MAGS_RAW_TO_PROCESSED_ROLE},
-)
+# Run each set of paths in parallel and set rebuild of databases downstream
+for i, flt in enumerate(file_land_timestamps) :
+    task_id = f"process-tar-{i}"
+    tasks[task_id] = KubernetesPodOperator(
+        dag=dag,
+        namespace="airflow",
+        image=MAGS_RAW_TO_PROCESSED_IMAGE,
+        env_vars={
+            "PYTHON_SCRIPT_NAME": TAR_PROCESS_SCRIPT,
+            "DB_VERSION": DB_VERSION,
+            "S3_RELATIVE_FOLDER_PATHS": flt,
+        },
+        arguments=["{{ ds }}"],
+        labels={"app": dag.dag_id},
+        name=task_id,
+        in_cluster=True,
+        task_id=task_id,
+        get_logs=True,
+        annotations={"iam.amazonaws.com/role": MAGS_RAW_TO_PROCESSED_ROLE},
+    )
 
-# Set dependencies
-tasks['process-tar'] >> tasks["rebuild-athena-schemas"]
+    # Set dependencies
+    tasks[task_id] >> tasks['rebuild_athena_schemas']
+
