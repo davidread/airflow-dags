fcdf1ba5b986c197a04f69ccdfdd90df18a7ab42
Karik Isichei <karik.isichei@gmail.com>
2018-11-22 13:08:49 +0000

    adding new DAG for curated databases and making changes to process to include hocas

--- mags_curated.py
+++ mags_curated.py
@@ -10,20 +10,15 @@ DB_VERSION='v1'
 MAGS_IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-magistrates-data-engineering:v0.0.6"
 MAGS_ROLE = "airflow_mags_data_processor"
 
-# TAR/HOCAS PROCESS SCRIPT ENVs
-S3_RELATIVE_FOLDER_PATHS = ""
-TAR_PROCESS_SCRIPT = "tar_raw_to_process.py"
-HOCAS_PROCESS_SCRIPT = "hocas_raw_to_process.py"
-
-# rebuild_databases.py tar_raw_to_process.py hocas_raw_to_process.py run_glue_job.py
+go_time = datetime.now()
+go_time.strftime("%Y-%m-%dT%H:%M:%S")
 
 # TAR GLUE JOB SCRIPT ENVs
 GLUE_JOB_BUCKET="alpha-mojap-curated-mags"
-GLUE_JOB_ROLE="alpha_user_isichei"
 AIRFLOW_JOB_NAME="airflow_curated_tar"
 GLUE_JOB_FOLDER_NAME="curated_tar"
-DATABASE_RUN_DATETIME="2018-11-16T00:00:00"
-SNAPSHOT_DATE="2018-11-16"
+DATABASE_RUN_DATETIME=go_time.strftime("%Y-%m-%dT%H:%M:%S")
+SNAPSHOT_DATE=go_time.strftime("%Y-%m-%d")
 ALLOCATED_CAPACITY="8"
 
 # SCHEMA REBUILD ENVs
@@ -40,7 +35,7 @@ task_args = {
 # Define process dag that just processes raw tar and hocas data in parallel.
 # Once complete rebuild processed DB only
 dag = DAG(
-    "mags_data_raw_to_processed",
+    "mags_data_curated",
     default_args=task_args,
     description="Process mags data (HOCAS and TAR)",
     start_date=datetime.now(),
@@ -49,33 +44,19 @@ dag = DAG(
 
 tasks = {}
 
-task_id = "process-tar"
+task_id = "tar-curated"
 tasks[task_id] = KubernetesPodOperator(
     dag=dag,
     namespace="airflow",
     image=MAGS_IMAGE,
     env_vars={
-        "PYTHON_SCRIPT_NAME": TAR_PROCESS_SCRIPT,
-        "S3_RELATIVE_FOLDER_PATHS": S3_RELATIVE_FOLDER_PATHS,
-        "DB_VERSION": DB_VERSION
-    },
-    arguments=["{{ ds }}"],
-    labels={"app": dag.dag_id},
-    name=task_id,
-    in_cluster=True,
-    task_id=task_id,
-    get_logs=True,
-    annotations={"iam.amazonaws.com/role": MAGS_ROLE},
-)
-
-task_id = "process-hocas"
-tasks[task_id] = KubernetesPodOperator(
-    dag=dag,
-    namespace="airflow",
-    image=MAGS_IMAGE,
-    env_vars={
-        "PYTHON_SCRIPT_NAME": HOCAS_PROCESS_SCRIPT,
-        "S3_RELATIVE_FOLDER_PATHS": S3_RELATIVE_FOLDER_PATHS,
+        "GLUE_JOB_BUCKET": GLUE_JOB_BUCKET,
+        "GLUE_JOB_ROLE": MAGS_ROLE,
+        "AIRFLOW_JOB_NAME": AIRFLOW_JOB_NAME,
+        "GLUE_JOB_FOLDER_NAME": GLUE_JOB_FOLDER_NAME,
+        "DATABASE_RUN_DATETIME": DATABASE_RUN_DATETIME,
+        "SNAPSHOT_DATE": SNAPSHOT_DATE,
+        "ALLOCATED_CAPACITY": ALLOCATED_CAPACITY,
         "DB_VERSION": DB_VERSION
     },
     arguments=["{{ ds }}"],
@@ -107,5 +88,4 @@ tasks[task_id] = KubernetesPodOperator(
 )
 
 # Set dependencies
-tasks['process-tar'] >> tasks["rebuild-athena-schemas"]
-tasks['process-hocas'] >> tasks["rebuild-athena-schemas"]
+tasks['curated-tar'] >> tasks["rebuild-athena-schemas"]
