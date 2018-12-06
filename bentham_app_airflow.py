b7acc63a14da7483c2d71e3cd5926492ecbe83d2
joeprinold <35369456+joeprinold@users.noreply.github.com>
2018-12-06 14:34:28 +0000

    Update bentham_app_airflow.py

--- bentham_app_airflow.py
+++ bentham_app_airflow.py
@@ -6,7 +6,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.utils.dates import days_ago
 
 # Define your docker image and the AWS role that will run the image (based on your airflow-repo)
-IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-bentham-app:v0.1.0"
+IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-bentham-app:v0.1.1"
 ROLE = "airflow_bentham_app"
 
 # Task arguments
