804acdaf8544e032e8468d11be37f082530abbd7
Sam Tazzyman <samtazzyman@sams-mbp.ws.local>
2018-12-20 11:04:36 +0000

    Changes to psst

--- psst.py
+++ psst.py
@@ -6,15 +6,15 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.utils.dates import days_ago
 
 # Define your docker image and the AWS role that will run the image (based on your airflow-repo)
-IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-oral-pqs:v1.0.3"
-ROLE = "airflow_oral_pqs"
+IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-psst-data:v0.0.2"
+ROLE = "airflow_psst_data"
 
 # Task arguments
 task_args = {
     "depends_on_past": False,
     "email_on_failure": True,
-    "owner": "samnlindsay",
-    "email": ["sam.lindsay@digital.justice.gov.uk"],
+    "owner": "samtazzyman",
+    "email": ["samuel.tazzyman@digital.justice.gov.uk"],
 }
 
 # # # Define your DAG
@@ -23,21 +23,19 @@ task_args = {
 # To actually put it on a schedule you can set something like:
 # start_date=datetime(2018, 8, 1), schedule_interval=timedelta(days=1)
 dag = DAG(
-    "oral_pqs",
+    "psst_data",
     default_args=task_args,
-    description="Go get some oral PQs and put them in S3",
-    start_date=datetime(2018, 11, 28),
-    schedule_interval=None
+    description="get new prison reports, process them, and put them in the psst",
+    start_date=datetime.now(),
+    schedule_interval==None
 )
 
-task_id = "oral-pqs-api-call"
+task_id = "psst-data"
 task1 = KubernetesPodOperator(
     dag=dag,
     namespace="airflow",
     image=IMAGE,
     env_vars={
-        "SCRIPT_NAME": "Oral_PQs_api_call.py",
-        "script1_var": '100'
     },
     labels={"app": dag.dag_id},
     name=task_id,
