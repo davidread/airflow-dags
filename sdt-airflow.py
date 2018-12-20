6c7d8ee9f385a05516722e5689b3745cea0e8ea0
filippoberio <philstevedent@gmail.com>
2018-12-20 14:51:48 +0000

    Update sdt-airflow.py

--- sdt-airflow.py
+++ sdt-airflow.py
@@ -6,7 +6,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.utils.dates import days_ago
 
 # Define your docker image and the AWS role that will run the image (based on your airflow-repo)
-IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-sdt:v0.8"
+IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-sdt:v0.9"
 ROLE = "airflow_sdt"
 
 # Task arguments
