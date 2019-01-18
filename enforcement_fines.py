fef9f9b605dd1e49920a8925fd12e78519db495e
Jamie Fraser <jamiefraser2@gmail.com>
2019-01-18 16:19:02 +0000

    add neater version of script

--- enforcement_fines.py
+++ enforcement_fines.py
@@ -9,7 +9,7 @@ from airflow.utils.dates import days_ago
 IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-enforcement-data-engineering:v0.0.4"
 ROLE = "airflow_enforcement_data_processing"
 
-#DATASET='closed'
+FINES_DATASET=['closed', 'transations', 'live']
 YEAR='2018'
 MONTH='10'
 BUCKET='alpha-enforcement-data-engineering'
@@ -35,51 +35,15 @@ dag = DAG(
     schedule_interval=None
 )
 
-task_id = "enforcement-fines-data-closed"
-task = KubernetesPodOperator(
-    dag=dag,
-    namespace="airflow",
-    image=IMAGE,
-    env_vars={
-        "DATASET": 'closed',
-        "YEAR": YEAR,
-        "MONTH": MONTH,
-        "BUCKET": BUCKET,
-    },
-    labels={"app": dag.dag_id},
-    name=task_id,
-    in_cluster=True,
-    task_id=task_id,
-    get_logs=True,
-    annotations={"iam.amazonaws.com/role": ROLE},
-)
 
-task_id = "enforcement-fines-data-transactions"
-task = KubernetesPodOperator(
+for dataset in FINES_DATASET:
+    task_id = '"{0}"'.format(''.join(['enforcement-fines-data-', dataset]))
+    task = KubernetesPodOperator(
     dag=dag,
     namespace="airflow",
     image=IMAGE,
     env_vars={
-        "DATASET": 'transactions',
-        "YEAR": YEAR,
-        "MONTH": MONTH,
-        "BUCKET": BUCKET,
-    },
-    labels={"app": dag.dag_id},
-    name=task_id,
-    in_cluster=True,
-    task_id=task_id,
-    get_logs=True,
-    annotations={"iam.amazonaws.com/role": ROLE},
-)
-
-task_id = "enforcement-fines-data-live"
-task = KubernetesPodOperator(
-    dag=dag,
-    namespace="airflow",
-    image=IMAGE,
-    env_vars={
-        "DATASET": 'live',
+        "DATASET": '"{0}"'.format(''.join(dataset)),
         "YEAR": YEAR,
         "MONTH": MONTH,
         "BUCKET": BUCKET,
