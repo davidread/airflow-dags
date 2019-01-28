24fbb94a6886d1eab4bbb02ab28392cd4e01fe41
MandarinDuck <duck@duck.local>
2019-01-28 16:34:22 +0000

    added release tag

--- nomis_transform.py
+++ nomis_transform.py
@@ -29,7 +29,7 @@ dag = DAG(
 
 # Define docker image and the AWS role (based on the airflow-repo)
 repo_name = "airflow-nomis-transform"
-repo_release_tag = ""
+repo_release_tag = "v2.0.0"
 IMAGE = f"593291632749.dkr.ecr.eu-west-1.amazonaws.com/{repo_name}:{repo_release_tag}"
 ROLE = "airflow-nomis-transform"
 
