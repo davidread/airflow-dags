3848d88d0673115f5926d8a866b9baf5a8f89c49
Jamie Fraser <jamiefraser2@gmail.com>
2019-01-30 12:52:02 +0000

    remove boto3 and re import statement

--- enforcement_fines.py
+++ enforcement_fines.py
@@ -4,8 +4,8 @@ import airflow
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-import boto3
-import re
+#import boto3
+#import re
 
 # Define your docker image and the AWS role that will run the image (based on your airflow-repo)
 IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-enforcement-data-engineering:v0.0.4"
