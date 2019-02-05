1b35102bafeb166ada7d67207c64f1b03ef9e180
Karik Isichei <karik.isichei@gmail.com>
2019-02-05 18:08:51 +0000

    updating to improved IAM version

--- nomis_daily_extraction.py
+++ nomis_daily_extraction.py
@@ -6,7 +6,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.utils.dates import days_ago
 
 # GLOBAL ENV VARIABLES
-IMAGE_VERSION = "v2.0.2"
+IMAGE_VERSION = "v2.0.3"
 IMAGE = f"593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-nomis-ap:{IMAGE_VERSION}"
 ROLE = "airflow_nomis_extraction"
 NOMIS_T62_FETCH_SIZE = '100000'
